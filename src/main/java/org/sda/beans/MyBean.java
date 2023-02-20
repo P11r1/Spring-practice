76f28c9414d90de5c3a9078e9855ad65ac320ebe
Marko <Piirmarko92@gmail.com>
2023-02-20 20:42:55 +0200

    Commit

---
+++ src/main/java/org/sda/beans/MyBean.java
@@ -0,0 +1,13 @@
+package org.sda.beans;
+
+
+import lombok.Setter;
+
+@Setter
+public class MyBean {
+    private String  name;
+
+    public String sayHello() {
+        return "Hello!" + name;
+    }
+}
